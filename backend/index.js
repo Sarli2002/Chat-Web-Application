5a8a547159d2f59a4f98c1cdef711158d74996ed
Sarli <sarlisengar@gmail.com>
2024-09-24 20:08:54 +0530

    backend updated

--- backend/index.js
+++ backend/index.js
@@ -29,9 +29,8 @@ app.use(cors({
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
 }).then(() => console.log('MongoDB connected')).catch(err => console.log(err));
+
 
 // User Schema
 const UserSchema = new mongoose.Schema({
